af2a79ded1552eba6ee449b66670c322e00c3861
Ricardo Hurtado <ricardo040400@gmail.com>
2024-03-07 21:31:19 -0400

    feat: Slidable aplicado a lista de producto

--- lib/src/widgets/menu/check_out_card.dart
+++ lib/src/widgets/menu/check_out_card.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:jarv/src/utils/models/producto_preordenado.dart';
 
 class CheckOut extends StatelessWidget {
@@ -18,7 +20,10 @@ class CheckOut extends StatelessWidget {
       required this.cantidad,
       this.backspace,
       required this.showTeclado,
-      this.dropDownIcon});
+      this.dropDownIcon,
+      this.deslizarItem,
+      required this.selectedItemLista,
+      this.onTapItem});
 
   final bool mostrarIdentificador;
   final bool showTeclado;
@@ -27,6 +32,7 @@ class CheckOut extends StatelessWidget {
   final ProductoPreOrdenado productoPreOrdenado;
   final List<String> cantidad;
   final Future<void> actualizarCantidad;
+  final ValueNotifier<int?> selectedItemLista;
   final dynamic onTextIdentificadorTap;
   final dynamic onBackIdentificador;
   final dynamic onAceptarIdentificador;
@@ -35,35 +41,38 @@ class CheckOut extends StatelessWidget {
   final dynamic addAction;
   final dynamic backspace;
   final dynamic dropDownIcon;
+  final dynamic deslizarItem;
+  final dynamic onTapItem;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      shape: RoundedRectangleBorder(
-          side: const BorderSide(color: Color.fromARGB(59, 7, 7, 7)),
-          borderRadius: BorderRadius.circular(20)),
+      shape: const RoundedRectangleBorder(
+          side: BorderSide(color: Color.fromARGB(59, 7, 7, 7)),
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
       elevation: 7,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          _listaProductoOrdenado(),
+          _listaProductoOrdenado(context),
           _totalVenta(),
           _elementoPreOrdenado(),
-          _teclado(context)
+          _teclado(context),
         ],
       ),
     );
   }
 
-  Expanded _listaProductoOrdenado() {
+  Expanded _listaProductoOrdenado(context) {
     return Expanded(
       child: mostrarIdentificador
           ? Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Padding(
                   padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 2.0),
+                      horizontal: 20.0, vertical: 5.0),
                   child: TextFormField(
                     onFieldSubmitted: (value) {
                       if (value.isNotEmpty) {
@@ -84,8 +93,8 @@ class CheckOut extends StatelessWidget {
                         child: const Text('Volver')),
                     ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.deepPurpleAccent,
-                            foregroundColor: Colors.white),
+                            backgroundColor: Theme.of(context).primaryColor,
+                            foregroundColor: Theme.of(context).canvasColor),
                         onPressed: () {
                           onAceptarIdentificador();
                         },
@@ -101,28 +110,57 @@ class CheckOut extends StatelessWidget {
                 final double total =
                     double.parse(producto!.cantidad) * producto.precio;
 
-                return Column(
-                  children: [
-                    Row(
+                return Slidable(
+                  key: UniqueKey(),
+                  startActionPane: ActionPane(
+                      extentRatio: 0.75,
+                      motion: const ScrollMotion(),
+                      openThreshold: 0.25,
                       children: [
-                        Expanded(
-                          child: ListTile(
-                            visualDensity: VisualDensity.compact,
-                            title: Text(producto.nombreProducto),
-                            subtitle: Text(
-                                '${producto.cantidad} x ${producto.precio}€'),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(total.toString()),
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                    )
-                  ],
+                        SlidableAction(
+                            onPressed: (context) {},
+                            backgroundColor: Theme.of(context)
+                                .colorScheme
+                                .secondaryContainer,
+                            foregroundColor: Theme.of(context)
+                                .colorScheme
+                                .onSecondaryContainer,
+                            icon: Icons.movie_edit,
+                            label: 'Precio'),
+                        SlidableAction(
+                            onPressed: (context) {},
+                            backgroundColor:
+                                Theme.of(context).colorScheme.tertiaryContainer,
+                            foregroundColor: Theme.of(context)
+                                .colorScheme
+                                .onTertiaryContainer,
+                            icon: Icons.delete,
+                            label: 'Eliminar'),
+                        SlidableAction(
+                            onPressed: (context) {},
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primaryContainer,
+                            foregroundColor: Theme.of(context)
+                                .colorScheme
+                                .onPrimaryContainer,
+                            icon: Icons.onetwothree_rounded,
+                            label: 'Unidades'),
+                      ]),
+                  child: Builder(builder: (context) {
+                    return ListTile(
+                      onTap: () {
+                        final slide = Slidable.of(context);
+                        slide!.openStartActionPane();
+                      },
+                      selected: selectedItemLista.value == index ? true : false,
+                      selectedTileColor: ThemeData().focusColor,
+                      visualDensity: VisualDensity.compact,
+                      title: Text(producto.nombreProducto),
+                      subtitle:
+                          Text('${producto.cantidad} x ${producto.precio}€'),
+                      trailing: Text('${total.toString()}€'),
+                    );
+                  }),
                 );
               },
             ),
@@ -190,6 +228,8 @@ class CheckOut extends StatelessWidget {
   }
 
   Widget _teclado(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 0.2;
+    final width = MediaQuery.of(context).size.width * 0.3;
     return Visibility(
       visible: showTeclado,
       child: Row(
@@ -199,15 +239,17 @@ class CheckOut extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.2,
-                width: MediaQuery.of(context).size.width * 0.3,
+                height: height,
+                width: width,
                 child: GridView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 0,
-                      crossAxisSpacing: 0,
-                      mainAxisExtent:
-                          MediaQuery.of(context).size.height * 0.07),
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 0,
+                    crossAxisSpacing: 0,
+                    mainAxisExtent: height / 3,
+                  ),
                   clipBehavior: Clip.antiAlias,
                   children: [
                     _itemNumeroTeclado('1'),
@@ -222,49 +264,68 @@ class CheckOut extends StatelessWidget {
                   ],
                 ),
               ),
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                ),
-                child: Container(
-                    color: const Color.fromARGB(115, 235, 235, 235),
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    height: MediaQuery.of(context).size.height * 0.08,
-                    child: _itemNumeroTeclado('0')),
+              Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20)),
+                    child: SizedBox(
+                      height: height / 3,
+                      width: width / 3,
+                      child: GestureDetector(
+                        onTap: () => addAction(cantidad),
+                        child: _itemActionTeclado(
+                          Icons.add,
+                          ThemeData().primaryColor.withOpacity(0.5),
+                          Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                      height: height / 3,
+                      width: width / 3,
+                      child: _itemNumeroTeclado('0')),
+                  GestureDetector(
+                    onTap: () => backspace(cantidad),
+                    child: SizedBox(
+                      height: height / 3,
+                      width: width / 3,
+                      child: _itemActionTeclado(
+                        Icons.backspace_outlined,
+                        ThemeData().highlightColor,
+                        Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
           Expanded(
             child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                bottomRight: Radius.circular(20),
-              ),
-              child: Column(
-                children: [
-                  GestureDetector(
-                    onTap: () => backspace(cantidad),
-                    child: SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.14,
-                        child: _itemActionTeclado(
-                            Icons.backspace_outlined,
-                            Colors.deepPurple[200]!,
-                            Colors.black,
-                            Colors.transparent)),
+              borderRadius:
+                  const BorderRadius.only(bottomRight: Radius.circular(20)),
+              child: SizedBox(
+                height: height + height / 3,
+                child: GestureDetector(
+                  onTap: () {
+                    showCupertinoModalPopup(
+                        barrierColor: const Color.fromARGB(29, 0, 0, 0),
+                        context: context,
+                        builder: (context) {
+                          return const ActionButton();
+                        });
+                  },
+                  child: _itemActionTeclado(
+                    Icons.view_agenda_outlined,
+                    ThemeData().primaryColor,
+                    Colors.white,
                   ),
-                  GestureDetector(
-                    onTap: () => addAction(cantidad),
-                    child: SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.14,
-                        child: _itemActionTeclado(
-                            Icons.add,
-                            Colors.deepPurple[500]!,
-                            Colors.white,
-                            Colors.transparent)),
-                  ),
-                ],
+                ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
@@ -272,29 +333,98 @@ class CheckOut extends StatelessWidget {
 
   Widget _itemNumeroTeclado(String label) {
     return GestureDetector(
-      onTap: () => onTapNum(label),
-      child: containerTeclado(
-          Text(label),
-          const Color.fromARGB(115, 235, 235, 235),
-          const Color.fromARGB(59, 7, 7, 7)),
+        onTap: () => onTapNum(label),
+        child: containerTeclado(
+            Text(
+              label,
+              style: const TextStyle(
+                  fontSize: 16.5,
+                  color: Color.fromARGB(255, 65, 65, 65),
+                  shadows: [
+                    Shadow(
+                      color: Color.fromARGB(
+                          38, 20, 6, 41), // Choose the color of the shadow
+                      blurRadius:
+                          5.0, // Adjust the blur radius for the shadow effect
+                      offset: Offset(1.0,
+                          1.0), // Set the horizontal and vertical offset for the shadow
+                    ),
+                  ]),
+            ),
+            const Color.fromARGB(115, 235, 235, 235)));
+  }
+
+  Widget _itemActionTeclado(IconData icon, Color color, Color colorIcon) {
+    return containerTeclado(
+      Icon(
+        icon,
+        color: colorIcon,
+      ),
+      color,
     );
   }
 
-  Widget _itemActionTeclado(
-      IconData icon, Color color, Color colorIcon, Color colorBorder) {
-    return containerTeclado(
-        Icon(
-          icon,
-          color: colorIcon,
-        ),
-        color,
-        colorBorder);
-  }
-
-  Container containerTeclado(Widget element, Color color, Color colorBorder) {
+  Container containerTeclado(Widget element, Color color) {
     return Container(
         decoration: BoxDecoration(
-            color: color, border: Border.all(color: colorBorder, width: 0.5)),
+            color: color,
+            border: Border.all(
+                color: const Color.fromARGB(59, 7, 7, 7), width: 0.15)),
         child: Center(child: element));
+  }
+}
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(28, 0, 0, 0),
+            offset: Offset(0, -5),
+            blurRadius: 45,
+          ),
+        ],
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
+        color: Colors.grey[300],
+      ),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.175,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.groups_rounded),
+              label: const Text('Cliente')),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.dinner_dining_outlined),
+              label: const Text('Consumicion Propia')),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.calendar_today_rounded),
+              label: const Text('Ticket Diario')),
+          ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/espera');
+              },
+              icon: const Icon(Icons.list_alt_rounded),
+              label: const Text('Venta en Espera')),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.account_balance_outlined),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  foregroundColor: Theme.of(context).canvasColor),
+              label: const Text('Cierre Diario')),
+        ],
+      ),
+    );
   }
 }
