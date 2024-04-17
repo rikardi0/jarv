b5222651e251e3c7318e00d790f6606fcedfb689
Ricardo Hurtado <ricardo040400@gmail.com>
2024-04-16 21:41:33 -0400

    feat: DAO y database para proveedor

--- lib/app/feature/proveedor/data/repositories/interfaces/proveedor_repository.dart
+++ lib/app/feature/proveedor/data/repositories/interfaces/proveedor_repository.dart
@@ -1,6 +1,11 @@
 import 'package:jarv/app/feature/proveedor/data/model/entity_proveedor.dart';
 
+import '../../../../../../shared/data/model/entity.dart';
+
 abstract class ProveedorRepository {
   Future<List<Proveedor>> findAllProveedores();
+  Stream<List<FamiliaProveedor>> findFamiliaByCif(String cif);
+  Stream<Familia?> findFamiliaById(String id);
   Future<void> insertProveedor(Proveedor proveedor);
+  Future<void> insertFamiliaProveedor(FamiliaProveedor familia);
 }
