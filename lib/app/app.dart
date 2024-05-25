4fc914f420e1e1c6528bf4d7991f21ed486c0397
Ricardo Hurtado <ricardo040400@gmail.com>
2024-05-25 17:16:53 -0400

    feat: Rutas actualizadas y limpieza de comentarios

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,15 +4,15 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:jarv/app/feature/inventario/ui/view/inventario_view.dart';
 import 'package:jarv/app/feature/login/ui/view/login.dart';
+import 'package:jarv/app/feature/login/ui/view/primer_registro.dart';
 import 'package:jarv/app/feature/proveedor/ui/view/proveedor_field.dart';
 import 'package:jarv/app/feature/proveedor/ui/view/proveedor_view.dart';
 import 'package:jarv/app/feature/settings/ui/provider/settings_controller.dart';
 import 'package:jarv/app/feature/settings/ui/view/dispositivos_settings_view.dart';
-import 'package:jarv/app/feature/settings/ui/view/empresa_settings_view.dart';
 import 'package:jarv/app/feature/settings/ui/view/exportar_settings_view.dart';
+import 'package:jarv/app/feature/settings/ui/view/institucion_settings_view.dart';
 import 'package:jarv/app/feature/settings/ui/view/personalizacion_settings_view.dart';
 import 'package:jarv/app/feature/settings/ui/view/settings_view.dart';
-import 'package:jarv/app/feature/settings/ui/view/tienda_settings_view.dart';
 import 'package:jarv/app/feature/settings/ui/view/time_settings_view.dart';
 import 'package:jarv/app/feature/settings/ui/view/ventas_settings_view.dart';
 import 'package:jarv/app/feature/venta/ui/view/cliente_field.dart';
@@ -38,47 +38,43 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.landscapeRight,
     ]);
 
-    return AnimatedBuilder(
-      animation: settingsController,
-      builder: (BuildContext context, Widget? child) {
-        return MultiProvider(
-          providers: [
-            ChangeNotifierProvider(
-                create: (BuildContext context) => VentaEsperaProvider(
-                      listaEspera: [],
-                      posicionListaEspera: null,
-                      mostrarElementoEspera: false,
-                    ))
-          ],
-          child: MaterialApp(
-            restorationScopeId: 'app',
-
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
+    return MediaQuery(
+      data: MediaQuery.of(context)
+          .copyWith(textScaler: const TextScaler.linear(1)),
+      child: AnimatedBuilder(
+        animation: settingsController,
+        builder: (BuildContext context, Widget? child) {
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                  create: (BuildContext context) => VentaEsperaProvider(
+                        listaEspera: [],
+                        posicionListaEspera: null,
+                        mostrarElementoEspera: false,
+                      ))
             ],
-            supportedLocales: const [
-              Locale('en', ''), // English, no country code
-            ],
-
-            // Use AppLocalizations to configure the correct application title
-            // depending on the user's locale.
-            //
-            // The appTitle is defined in .arb files found in the localization
-            // directory.
-            onGenerateTitle: (BuildContext context) =>
-                AppLocalizations.of(context)!.appTitle,
-
-            theme: CustomThemeData.lightTheme(context),
-            darkTheme: CustomThemeData.darkTheme(context),
-            themeMode: settingsController.themeMode,
-
-            onGenerateRoute: route,
-          ),
-        );
-      },
+            child: MaterialApp(
+              restorationScopeId: 'app',
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: const [
+                Locale('es'), // Spanish no country code
+                Locale('en'), // English
+              ],
+              onGenerateTitle: (BuildContext context) =>
+                  AppLocalizations.of(context)!.appTitle,
+              theme: CustomThemeData.lightTheme(context),
+              darkTheme: CustomThemeData.darkTheme(context),
+              themeMode: settingsController.themeMode,
+              onGenerateRoute: route,
+            ),
+          );
+        },
+      ),
     );
   }
 
@@ -104,22 +100,23 @@ class MyApp extends StatelessWidget {
     };
 
     final Map<String, WidgetBuilder> settingsRoutes = {
-      SettingsView.routeName: (context) =>
-          SettingsView(controller: settingsController),
-      EmpresaSettingsView.routeName: (context) => const EmpresaSettingsView(),
-      TiendaSettingsView.routeName: (context) => const TiendaSettingsView(),
+      SettingsView.routeName: (context) => const SettingsView(),
+      InstitucionSettingsView.routeName: (context) =>
+          const InstitucionSettingsView(),
       PersonalizacionSettingsView.routeName: (context) =>
-          const PersonalizacionSettingsView(),
+          PersonalizacionSettingsView(
+            controller: settingsController,
+          ),
       DispositivosSettingsView.routeName: (context) =>
           const DispositivosSettingsView(),
-      VentasSettingsView.routeName: (context) => VentasSettingsView(),
-      TimeSettingsView.routeName: (context) => TimeSettingsView(),
-      ExportarSettingsView.routeName: (context) => ExportarSettingsView(),
+      VentasSettingsView.routeName: (context) => const VentasSettingsView(),
+      TimeSettingsView.routeName: (context) => const TimeSettingsView(),
+      ExportarSettingsView.routeName: (context) => const ExportarSettingsView(),
     };
 
     final Map<String, WidgetBuilder> routeBuilders = {
       ...loginRoutes,
-      ...ventaRoutes, // Spread operator to merge maps
+      ...ventaRoutes,
       ...proveedorRoutes,
       ...settingsRoutes,
     };
@@ -129,8 +126,11 @@ class MyApp extends StatelessWidget {
         settings: routeSettings,
         builder: routeBuilder,
       );
+    } else {
+      return MaterialPageRoute(
+        builder: (context) => const PrimerRegistro(),
+        settings: routeSettings,
+      );
     }
-
-    return null;
   }
 }
